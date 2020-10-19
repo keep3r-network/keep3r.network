fe0fbde97d29e737a5bcffb88d2d29157268274c
Andre Cronje <andre.cronje@yearn.finance>
2020-10-19 15:56:33 +0200

    Add time delay to credit Add delegation and voting rights only to bonded keepers
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- contracts/Keep3r.sol
+++ contracts/Keep3r.sol
@@ -243,17 +243,167 @@ contract Keep3r {
     /// @notice Total number of tokens in circulation
     uint public totalSupply = 0; // Initial 0
 
+    /// @notice A record of each accounts delegate
+    mapping (address => address) public delegates;
+
+    /// @notice A record of votes checkpoints for each account, by index
+    mapping (address => mapping (uint32 => Checkpoint)) public checkpoints;
+
+    /// @notice The number of checkpoints for each account
+    mapping (address => uint32) public numCheckpoints;
+
     mapping (address => mapping (address => uint)) internal allowances;
     mapping (address => uint) internal balances;
 
     /// @notice The EIP-712 typehash for the contract's domain
     bytes32 public constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint chainId,address verifyingContract)");
 
+    /// @notice The EIP-712 typehash for the delegation struct used by the contract
+    bytes32 public constant DELEGATION_TYPEHASH = keccak256("Delegation(address delegatee,uint nonce,uint expiry)");
+
     /// @notice The EIP-712 typehash for the permit struct used by the contract
     bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint value,uint nonce,uint deadline)");
 
+
     /// @notice A record of states for signing / validating signatures
     mapping (address => uint) public nonces;
+
+    /// @notice An event thats emitted when an account changes its delegate
+    event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
+
+    /// @notice An event thats emitted when a delegate account's vote balance changes
+    event DelegateVotesChanged(address indexed delegate, uint previousBalance, uint newBalance);
+
+    /// @notice A checkpoint for marking number of votes from a given block
+    struct Checkpoint {
+        uint32 fromBlock;
+        uint votes;
+    }
+
+    /**
+     * @notice Delegate votes from `msg.sender` to `delegatee`
+     * @param delegatee The address to delegate votes to
+     */
+    function delegate(address delegatee) public {
+        return _delegate(msg.sender, delegatee);
+    }
+
+    /**
+     * @notice Delegates votes from signatory to `delegatee`
+     * @param delegatee The address to delegate votes to
+     * @param nonce The contract state required to match the signature
+     * @param expiry The time at which to expire the signature
+     * @param v The recovery byte of the signature
+     * @param r Half of the ECDSA signature pair
+     * @param s Half of the ECDSA signature pair
+     */
+    function delegateBySig(address delegatee, uint nonce, uint expiry, uint8 v, bytes32 r, bytes32 s) public {
+        bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name)), getChainId(), address(this)));
+        bytes32 structHash = keccak256(abi.encode(DELEGATION_TYPEHASH, delegatee, nonce, expiry));
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
+        address signatory = ecrecover(digest, v, r, s);
+        require(signatory != address(0), "::delegateBySig: invalid signature");
+        require(nonce == nonces[signatory]++, "::delegateBySig: invalid nonce");
+        require(now <= expiry, "::delegateBySig: signature expired");
+        return _delegate(signatory, delegatee);
+    }
+
+    /**
+     * @notice Gets the current votes balance for `account`
+     * @param account The address to get votes balance
+     * @return The number of current votes for `account`
+     */
+    function getCurrentVotes(address account) external view returns (uint) {
+        uint32 nCheckpoints = numCheckpoints[account];
+        return nCheckpoints > 0 ? checkpoints[account][nCheckpoints - 1].votes : 0;
+    }
+
+    /**
+     * @notice Determine the prior number of votes for an account as of a block number
+     * @dev Block number must be a finalized block or else this function will revert to prevent misinformation.
+     * @param account The address of the account to check
+     * @param blockNumber The block number to get the vote balance at
+     * @return The number of votes the account had as of the given block
+     */
+    function getPriorVotes(address account, uint blockNumber) public view returns (uint) {
+        require(blockNumber < block.number, "::getPriorVotes: not yet determined");
+
+        uint32 nCheckpoints = numCheckpoints[account];
+        if (nCheckpoints == 0) {
+            return 0;
+        }
+
+        // First check most recent balance
+        if (checkpoints[account][nCheckpoints - 1].fromBlock <= blockNumber) {
+            return checkpoints[account][nCheckpoints - 1].votes;
+        }
+
+        // Next check implicit zero balance
+        if (checkpoints[account][0].fromBlock > blockNumber) {
+            return 0;
+        }
+
+        uint32 lower = 0;
+        uint32 upper = nCheckpoints - 1;
+        while (upper > lower) {
+            uint32 center = upper - (upper - lower) / 2; // ceil, avoiding overflow
+            Checkpoint memory cp = checkpoints[account][center];
+            if (cp.fromBlock == blockNumber) {
+                return cp.votes;
+            } else if (cp.fromBlock < blockNumber) {
+                lower = center;
+            } else {
+                upper = center - 1;
+            }
+        }
+        return checkpoints[account][lower].votes;
+    }
+
+    function _delegate(address delegator, address delegatee) internal {
+        address currentDelegate = delegates[delegator];
+        uint delegatorBalance = bonds[delegator];
+        delegates[delegator] = delegatee;
+
+        emit DelegateChanged(delegator, currentDelegate, delegatee);
+
+        _moveDelegates(currentDelegate, delegatee, delegatorBalance);
+    }
+
+    function _moveDelegates(address srcRep, address dstRep, uint amount) internal {
+        if (srcRep != dstRep && amount > 0) {
+            if (srcRep != address(0)) {
+                uint32 srcRepNum = numCheckpoints[srcRep];
+                uint srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
+                uint srcRepNew = srcRepOld.sub(amount, "::_moveVotes: vote amount underflows");
+                _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
+            }
+
+            if (dstRep != address(0)) {
+                uint32 dstRepNum = numCheckpoints[dstRep];
+                uint dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
+                uint dstRepNew = dstRepOld.add(amount);
+                _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
+            }
+        }
+    }
+
+    function _writeCheckpoint(address delegatee, uint32 nCheckpoints, uint oldVotes, uint newVotes) internal {
+      uint32 blockNumber = safe32(block.number, "::_writeCheckpoint: block number exceeds 32 bits");
+
+      if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
+          checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
+      } else {
+          checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, newVotes);
+          numCheckpoints[delegatee] = nCheckpoints + 1;
+      }
+
+      emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
+    }
+
+    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
+        require(n < 2**32, errorMessage);
+        return uint32(n);
+    }
 
     /// @notice The standard EIP-20 transfer event
     event Transfer(address indexed from, address indexed to, uint amount);
@@ -343,6 +493,8 @@ contract Keep3r {
     mapping(address => uint) public liquidityUnbonding;
     /// @notice job proposal delay
     mapping(address => uint) public jobProposalDelay;
+    /// @notice liquidity apply date
+    mapping(address => uint) public liquidityApplied;
 
     /// @notice list of all current keepers
     mapping(address => bool) public keepers;
@@ -383,17 +535,22 @@ contract Keep3r {
     function submitJob(address job, uint amount) external {
         require(liquidityProvided[msg.sender]==address(0x0), "Keep3r::submitJob: liquidity already provided, please remove first");
         liquidity.transferFrom(msg.sender, address(this), amount);
-        uint _liquidity = balances[address(liquidity)];
-        uint _credit = _liquidity.mul(amount).div(liquidity.totalSupply());
-        credits[job] = credits[job].add(_credit.mul(2)); // Double liquidity to account for 50:50
         liquidityProviders[msg.sender] = amount;
         liquidityProvided[msg.sender] = job;
-
+        liquidityApplied[msg.sender] = now.add(1 days);
         if (!jobs[job] && jobProposalDelay[job] < now) {
             Governance(governance).proposeJob(job);
             jobProposalDelay[job] = now.add(UNBOND);
         }
-        emit SubmitJob(job, msg.sender, block.number, _credit.mul(2));
+        emit SubmitJob(job, msg.sender, block.number, amount);
+    }
+
+    function credit(address provider, address job) external {
+        require(liquidityApplied[provider] != 0, "Keep3r::credit: submitJob first");
+        require(liquidityApplied[provider] < now, "Keep3r::credit: still bonding");
+        uint _liquidity = balances[address(liquidity)];
+        uint _credit = _liquidity.mul(liquidityProviders[provider]).div(liquidity.totalSupply());
+        credits[job] = credits[job].add(_credit.mul(2)); // Double liquidity to account for 50:50
     }
 
     /**
@@ -565,6 +722,7 @@ contract Keep3r {
     function unbond() external {
         keepers[msg.sender] = false;
         unbondings[msg.sender] = now.add(UNBOND);
+        _moveDelegates(delegates[msg.sender], address(0), bonds[msg.sender]);
         emit KeeperUnbonding(msg.sender, block.number, unbondings[msg.sender], bonds[msg.sender]);
     }
 
