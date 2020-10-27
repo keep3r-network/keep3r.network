4ab4562da7f9e7de8655195af3a5469014863065
Andre Cronje <andre.cronje@yearn.finance>
2020-10-27 06:19:54 +0200

    PVE002: safeTransferFrom() in bond()/addCredit()/addLiquidityToJob()
    Closes #6
    
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- contracts/Keep3r.sol
+++ contracts/Keep3r.sol
@@ -245,26 +245,233 @@ contract ReentrancyGuard {
     }
 }
 
-interface Governance {
+interface IGovernance {
     function proposeJob(address job) external returns (uint);
 }
 
-interface Keep3rHelper {
+interface IKeep3rHelper {
     function getQuoteLimit(uint gasUsed) external view returns (uint);
 }
 
-interface ERC20 {
-    function transfer(address to, uint value) external returns (bool);
-    function transferFrom(address from, address to, uint value) external returns (bool);
-    function balanceOf(address account) external view returns (uint);
-    function totalSupply() external view returns (uint);
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
+ * @dev Collection of functions related to the address type
+ */
+library Address {
+    /**
+     * @dev Returns true if `account` is a contract.
+     *
+     * [IMPORTANT]
+     * ====
+     * It is unsafe to assume that an address for which this function returns
+     * false is an externally-owned account (EOA) and not a contract.
+     *
+     * Among others, `isContract` will return false for the following
+     * types of addresses:
+     *
+     *  - an externally-owned account
+     *  - a contract in construction
+     *  - an address where a contract will be created
+     *  - an address where a contract lived, but was destroyed
+     * ====
+     */
+    function isContract(address account) internal view returns (bool) {
+        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
+        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
+        // for accounts without code, i.e. `keccak256('')`
+        bytes32 codehash;
+        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { codehash := extcodehash(account) }
+        return (codehash != accountHash && codehash != 0x0);
+    }
+
+    /**
+     * @dev Converts an `address` into `address payable`. Note that this is
+     * simply a type cast: the actual underlying value is not changed.
+     *
+     * _Available since v2.4.0._
+     */
+    function toPayable(address account) internal pure returns (address payable) {
+        return address(uint160(account));
+    }
+
+    /**
+     * @dev Replacement for Solidity's `transfer`: sends `amount` wei to
+     * `recipient`, forwarding all available gas and reverting on errors.
+     *
+     * https://eips.ethereum.org/EIPS/eip-1884[EIP1884] increases the gas cost
+     * of certain opcodes, possibly making contracts go over the 2300 gas limit
+     * imposed by `transfer`, making them unable to receive funds via
+     * `transfer`. {sendValue} removes this limitation.
+     *
+     * https://diligence.consensys.net/posts/2019/09/stop-using-soliditys-transfer-now/[Learn more].
+     *
+     * IMPORTANT: because control is transferred to `recipient`, care must be
+     * taken to not create reentrancy vulnerabilities. Consider using
+     * {ReentrancyGuard} or the
+     * https://solidity.readthedocs.io/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
+     *
+     * _Available since v2.4.0._
+     */
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Address: insufficient balance");
+
+        // solhint-disable-next-line avoid-call-value
+        (bool success, ) = recipient.call{value:amount}("");
+        require(success, "Address: unable to send value, recipient may have reverted");
+    }
+}
+
+/**
+ * @title SafeERC20
+ * @dev Wrappers around ERC20 operations that throw on failure (when the token
+ * contract returns false). Tokens that return no value (and instead revert or
+ * throw on failure) are also supported, non-reverting calls are assumed to be
+ * successful.
+ * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
+ * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
+ */
+library SafeERC20 {
+    using SafeMath for uint256;
+    using Address for address;
+
+    function safeTransfer(IERC20 token, address to, uint256 value) internal {
+        callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
+        callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    function safeApprove(IERC20 token, address spender, uint256 value) internal {
+        // safeApprove should only be called when setting an initial allowance,
+        // or when resetting it to zero. To increase and decrease it, use
+        // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
+        // solhint-disable-next-line max-line-length
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+
+    function safeIncreaseAllowance(IERC20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).add(value);
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    function safeDecreaseAllowance(IERC20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).sub(value, "SafeERC20: decreased allowance below zero");
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    /**
+     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+     * on the return value: the return value is optional (but if data is returned, it must not be false).
+     * @param token The token targeted by the call.
+     * @param data The call data (encoded using abi.encode or one of its variants).
+     */
+    function callOptionalReturn(IERC20 token, bytes memory data) private {
+        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
+        // we're implementing it ourselves.
+
+        // A Solidity high level call has three parts:
+        //  1. The target address is checked to verify it contains contract code
+        //  2. The call itself is made, and success asserted
+        //  3. The return value is decoded, which in turn checks the size of the returned data.
+        // solhint-disable-next-line max-line-length
+        require(address(token).isContract(), "SafeERC20: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = address(token).call(data);
+        require(success, "SafeERC20: low-level call failed");
+
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
 }
 
 contract Keep3r is ReentrancyGuard {
     using SafeMath for uint;
+    using SafeERC20 for IERC20;
 
     /// @notice Keep3r Helper to set max prices for the ecosystem
-    Keep3rHelper public KPRH;
+    IKeep3rHelper public KPRH;
 
     /// @notice EIP-20 token name for this token
     string public constant name = "Keep3r";
@@ -595,9 +802,9 @@ contract Keep3r is ReentrancyGuard {
      */
     function addCredit(address credit, address job, uint amount) external nonReentrant {
         require(jobs[job], "addCreditETH: !job");
-        uint _before = ERC20(credit).balanceOf(address(this));
-        ERC20(credit).transferFrom(msg.sender, address(this), amount);
-        uint _after = ERC20(credit).balanceOf(address(this));
+        uint _before = IERC20(credit).balanceOf(address(this));
+        IERC20(credit).safeTransferFrom(msg.sender, address(this), amount);
+        uint _after = IERC20(credit).balanceOf(address(this));
         credits[job][credit] = credits[job][credit].add(_after.sub(_before));
 
         emit AddCredit(credit, job, msg.sender, block.number, _after.sub(_before));
@@ -673,14 +880,14 @@ contract Keep3r is ReentrancyGuard {
      */
     function addLiquidityToJob(address liquidity, address job, uint amount) external nonReentrant {
         require(liquidityAccepted[liquidity], "addLiquidityToJob: !pair");
-        ERC20(liquidity).transferFrom(msg.sender, address(this), amount);
+        IERC20(liquidity).safeTransferFrom(msg.sender, address(this), amount);
         liquidityProvided[msg.sender][liquidity][job] = liquidityProvided[msg.sender][liquidity][job].add(amount);
 
         liquidityApplied[msg.sender][liquidity][job] = now.add(LIQUIDITYBOND);
         liquidityAmount[msg.sender][liquidity][job] = liquidityAmount[msg.sender][liquidity][job].add(amount);
 
         if (!jobs[job] && jobProposalDelay[job] < now) {
-            Governance(governance).proposeJob(job);
+            IGovernance(governance).proposeJob(job);
             jobProposalDelay[job] = now.add(UNBOND);
         }
         emit SubmitJob(job, msg.sender, block.number, amount);
@@ -697,7 +904,7 @@ contract Keep3r is ReentrancyGuard {
         require(liquidityApplied[provider][liquidity][job] != 0, "credit: no bond");
         require(liquidityApplied[provider][liquidity][job] < now, "credit: bonding");
         uint _liquidity = balances[address(liquidity)];
-        uint _credit = _liquidity.mul(liquidityAmount[msg.sender][liquidity][job]).div(ERC20(liquidity).totalSupply());
+        uint _credit = _liquidity.mul(liquidityAmount[msg.sender][liquidity][job]).div(IERC20(liquidity).totalSupply());
         _mint(address(this), _credit);
         credits[job][address(this)] = credits[job][address(this)].add(_credit);
         liquidityAmount[msg.sender][liquidity][job] = 0;
@@ -718,7 +925,7 @@ contract Keep3r is ReentrancyGuard {
         require(liquidityAmountsUnbonding[msg.sender][liquidity][job] <= liquidityProvided[msg.sender][liquidity][job], "unbondLiquidityFromJob: insufficient funds");
 
         uint _liquidity = balances[address(liquidity)];
-        uint _credit = _liquidity.mul(amount).div(ERC20(liquidity).totalSupply());
+        uint _credit = _liquidity.mul(amount).div(IERC20(liquidity).totalSupply());
         if (_credit > credits[job][address(this)]) {
             _burn(address(this), credits[job][address(this)]);
             credits[job][address(this)] = 0;
@@ -741,7 +948,7 @@ contract Keep3r is ReentrancyGuard {
         uint _amount = liquidityAmountsUnbonding[msg.sender][liquidity][job];
         liquidityProvided[msg.sender][liquidity][job] = liquidityProvided[msg.sender][liquidity][job].sub(_amount);
         liquidityAmountsUnbonding[msg.sender][liquidity][job] = 0;
-        ERC20(liquidity).transfer(msg.sender, _amount);
+        IERC20(liquidity).safeTransfer(msg.sender, _amount);
 
         emit RemoveJob(job, msg.sender, block.number, _amount);
     }
@@ -811,7 +1018,7 @@ contract Keep3r is ReentrancyGuard {
         require(jobs[msg.sender], "receipt: !job");
         credits[msg.sender][credit] = credits[msg.sender][credit].sub(amount, "workReceipt: insuffient funds");
         lastJob[keeper] = now;
-        ERC20(credit).transfer(msg.sender, amount);
+        IERC20(credit).safeTransfer(msg.sender, amount);
         emit KeeperWorked(credit, msg.sender, keeper, block.number);
     }
 
@@ -879,7 +1086,7 @@ contract Keep3r is ReentrancyGuard {
      * @notice Allows governance to change the Keep3rHelper for max spend
      * @param _kprh new helper address to set
      */
-    function setKeep3rHelper(Keep3rHelper _kprh) external {
+    function setKeep3rHelper(IKeep3rHelper _kprh) external {
         require(msg.sender == governance, "setKeep3rHelper: !gov");
         KPRH = _kprh;
     }
@@ -956,7 +1163,7 @@ contract Keep3r is ReentrancyGuard {
         if (bonding == address(this)) {
             _transferTokens(msg.sender, address(this), amount);
         } else {
-            ERC20(bonding).transferFrom(msg.sender, address(this), amount);
+            IERC20(bonding).safeTransferFrom(msg.sender, address(this), amount);
         }
         pendingbonds[msg.sender][bonding] = pendingbonds[msg.sender][bonding].add(amount);
         emit KeeperBonding(msg.sender, block.number, bondings[msg.sender][bonding], amount);
@@ -1010,7 +1217,7 @@ contract Keep3r is ReentrancyGuard {
         if (bonding == address(this)) {
             _transferTokens(address(this), msg.sender, partialUnbonding[msg.sender][bonding]);
         } else {
-            ERC20(bonding).transfer(msg.sender, partialUnbonding[msg.sender][bonding]);
+            IERC20(bonding).safeTransfer(msg.sender, partialUnbonding[msg.sender][bonding]);
         }
         emit KeeperUnbound(msg.sender, block.number, block.timestamp, partialUnbonding[msg.sender][bonding]);
         partialUnbonding[msg.sender][bonding] = 0;
@@ -1055,7 +1262,7 @@ contract Keep3r is ReentrancyGuard {
         if (bonded == address(this)) {
             _transferTokens(address(this), governance, amount);
         } else {
-            ERC20(bonded).transfer(governance, amount);
+            IERC20(bonded).safeTransfer(governance, amount);
         }
         _unbond(bonded, keeper, amount);
         disputes[keeper] = false;
