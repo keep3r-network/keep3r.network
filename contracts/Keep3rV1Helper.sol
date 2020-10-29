973a6c0c4efb656329e631f6f1199288d8b65bb5
Andre Cronje <andre.cronje@yearn.finance>
2020-10-29 11:18:10 +0200

    Helper updates
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- contracts/Keep3rV1Helper.sol
+++ contracts/Keep3rV1Helper.sol
@@ -197,8 +197,8 @@ contract Keep3rV1Helper {
     IChainLinkFeed public constant FASTGAS = IChainLinkFeed(0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C);
     IUniswapV2Oracle public constant UNIQUOTE = IUniswapV2Oracle(0x127a2975c4E1c75f1ed4757a861bbd42523DB035);
 
-    address constant WETH = address(0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44);
-    address constant KP3R = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    address constant WETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    address constant KP3R = address(0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44);
 
     function quote(uint spent) public view returns (uint) {
         return UNIQUOTE.consult(WETH, spent, KP3R);
