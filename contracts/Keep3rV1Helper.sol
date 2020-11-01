0cc93f616f84c41f513429d2d48c30bdc5693a80
Andre Cronje <andre.cronje@yearn.finance>
2020-11-01 11:09:12 +0200

    Keep3rV1Helper.sol
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- contracts/Keep3rV1Helper.sol
+++ contracts/Keep3rV1Helper.sol
@@ -183,32 +183,67 @@ library SafeMath {
     }
 }
 
+library Math {
+    /**
+     * @dev Returns the largest of two numbers.
+     */
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    /**
+     * @dev Returns the smallest of two numbers.
+     */
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    /**
+     * @dev Returns the average of two numbers. The result is rounded towards
+     * zero.
+     */
+    function average(uint256 a, uint256 b) internal pure returns (uint256) {
+        // (a + b) / 2 can overflow, so we distribute
+        return (a / 2) + (b / 2) + ((a % 2 + b % 2) / 2);
+    }
+}
+
 interface IChainLinkFeed {
     function latestAnswer() external view returns (int256);
 }
 
-interface IUniswapV2Oracle {
-    function consult(address tokenIn, uint amountIn, address tokenOut) external view returns (uint amountOut);
+interface IKeep3rV1 {
+    function totalBonded() external view returns (uint);
+    function bonds(address keeper, address credit) external view returns (uint);
+    function votes(address keeper) external view returns (uint);
 }
 
 contract Keep3rV1Helper {
     using SafeMath for uint;
 
     IChainLinkFeed public constant FASTGAS = IChainLinkFeed(0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C);
-    IUniswapV2Oracle public constant UNIQUOTE = IUniswapV2Oracle(0x127a2975c4E1c75f1ed4757a861bbd42523DB035);
+    IKeep3rV1 public constant KP3R = IKeep3rV1(0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44);
 
-    address constant WETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    address constant KP3R = address(0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44);
+    uint constant public BOOST = 25;
+    uint constant public BASE = 10;
+    uint constant public TARGETBOND = 100e18;
 
-    function quote(uint spent) public view returns (uint) {
-        return UNIQUOTE.consult(WETH, spent, KP3R);
+    function getFastGas() external view returns (uint) {
+        return uint(FASTGAS.latestAnswer());
     }
 
-    function quoteGas(uint gasUsed) external view returns (uint) {
-        return gasUsed.mul(uint(FASTGAS.latestAnswer()));
+    function bonds(address keeper) public view returns (uint) {
+        return KP3R.bonds(keeper, address(KP3R)).add(KP3R.votes(keeper));
+    }
+
+    function getQuoteLimitFor(address origin, uint gasUsed) public view returns (uint) {
+        uint _min = gasUsed.mul(uint(FASTGAS.latestAnswer()));
+        uint _boost = _min.mul(BOOST).div(BASE); // increase by 2.5
+        uint _bond = Math.min(bonds(origin), TARGETBOND);
+        return Math.max(_min, _boost.mul(_bond).div(TARGETBOND));
     }
 
     function getQuoteLimit(uint gasUsed) external view returns (uint) {
-        return quote(gasUsed.mul(uint(FASTGAS.latestAnswer())));
+        return getQuoteLimitFor(tx.origin, gasUsed);
     }
 }
