b8b3de0f7999c92e048d943a8f36ef625725be55
Andre Cronje <andre.cronje@yearn.finance>
2020-11-08 11:48:29 +0200

    Updated Helper model, rewards for bonded keepers are more favorable
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- contracts/Keep3rV1Helper.sol
+++ contracts/Keep3rV1Helper.sol
@@ -224,9 +224,11 @@ contract Keep3rV1Helper {
     IChainLinkFeed public constant FASTGAS = IChainLinkFeed(0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C);
     IKeep3rV1 public constant KP3R = IKeep3rV1(0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44);
 
-    uint constant public BOOST = 25;
+    uint constant public BOOST = 50;
     uint constant public BASE = 10;
-    uint constant public TARGETBOND = 100e18;
+    uint constant public TARGETBOND = 200e18;
+
+    uint constant public PRICE = 10;
 
     function getFastGas() external view returns (uint) {
         return uint(FASTGAS.latestAnswer());
@@ -237,7 +239,7 @@ contract Keep3rV1Helper {
     }
 
     function getQuoteLimitFor(address origin, uint gasUsed) public view returns (uint) {
-        uint _min = gasUsed.mul(uint(FASTGAS.latestAnswer()));
+        uint _min = gasUsed.mul(PRICE).mul(uint(FASTGAS.latestAnswer()));
         uint _boost = _min.mul(BOOST).div(BASE); // increase by 2.5
         uint _bond = Math.min(bonds(origin), TARGETBOND);
         return Math.max(_min, _boost.mul(_bond).div(TARGETBOND));
